a35a7eb95055cb97439a79fea330b3d23b3fbac0
seki221 <pawls00.bugaboo@icloud.com>
2024-11-27 22:53:36 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: %i[new create index show]
   root to: 'schedules#index'
-  resources :schedules
+  resources :schedules, only: %i[index new create show edit update destroy favorite] do
+    resources :comments, only: %i[update create edit destroy], shallow: true
+    collection do
+      get :favorite
+      get 'search'
+    end
+  end
+  
+  resources :favorite, only: %i[create destroy], shallow: true
 end
