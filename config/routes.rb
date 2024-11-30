3512781cab8a3274c2c39a9fc2b8b1bc998b36d1
seki221 <pawls00.bugaboo@icloud.com>
2024-11-30 23:40:31 +0900

    route変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root to: 'schedules#index'
-  resources :schedules, only: %i[index new create show edit update destroy favorite] do
+  resources :schedules, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[update create edit destroy], shallow: true
     collection do
       get :favorite
       get 'search'
     end
   end
-  
   resources :favorite, only: %i[create destroy], shallow: true
 end
