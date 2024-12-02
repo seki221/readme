d7121dc463852df626f840e9cb4bf9107fe2bd64
seki221 <pawls00.bugaboo@icloud.com>
2024-12-02 11:12:30 +0900

    詳細ページ変更

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,13 +1,20 @@
-
 class DatesController < ApplicationController
   def show
     if params[:date].present?
-      begin
-        @date = Date.parse(params[:date])
-      rescue ArgumentError
-        @date = Date.today
-      end
+      @date = parse_date(params[:date])
     else
       @date = Date.today
     end
+
+    @schedules_for_start_at = Schedule.where(date: @date).order(:start_at)
+    @dates = Schedule.distinct.pluck(:date).compact
+  end
+
+  private
+
+  def parse_date(date_param)
+    Date.parse(date_param)
+  rescue ArgumentError
+    Date.today
+  end
 end
