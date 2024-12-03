7b6a9df6e343e67f579fc4015f9274d41248df3b
seki221 <pawls00.bugaboo@icloud.com>
2024-12-03 09:42:25 +0900

    jsに時間系追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,16 @@ import * as ActiveStorage from "@rails/activestorage"
 
 Rails.start()
 ActiveStorage.start()
+
+document.addEventListener("turbo:submit-start", function(event) {
+  const button = event.target.querySelector("input[type='submit']");
+  if (button) button.disabled = true;
+});
+
+document.addEventListener("turbo:submit-end", function(event) {
+  if (event.detail.success === false) {
+    const button = event.target.querySelector("input[type='submit']");
+    if (button) button.disabled = false;
+  }
+});
+
