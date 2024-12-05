c7de855b4a53f4b8631655ccc95184a2677b304f
seki221 <pawls00.bugaboo@icloud.com>
2024-12-05 11:11:06 +0900

    ログイン管理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
+  before_action :debug_current_user
 
-  protected
+  private
+  def require_login
+    unless current_user
+      redirect_to login_path, alert: 'ログインが必要です。'
+    end
+  end
+
+  def debug_current_user
+    Rails.logger.debug "Current User: #{current_user.inspect}"
+  end
+
 
   def configure_permitted_parameters
     if resource_class == User
