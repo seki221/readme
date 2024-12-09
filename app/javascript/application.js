70e619e36321272ba26046f0eb448d3ebda1da98
seki221 <pawls00.bugaboo@icloud.com>
2024-12-09 21:19:28 +0900

    jsファイル移動

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,21 +3,3 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-
-Rails.start()
-ActiveStorage.start()
-
-document.addEventListener("turbo:submit-start", function(event) {
-  const button = event.target.querySelector("input[type='submit']");
-  if (button) button.disabled = true;
-});
-
-document.addEventListener("turbo:submit-end", function(event) {
-  if (event.detail.success === false) {
-    const button = event.target.querySelector("input[type='submit']");
-    if (button) button.disabled = false;
-  }
-});
-
