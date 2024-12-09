a030c8c5986e27b927a1829e8d240a340b9f1e46
seki221 <pawls00.bugaboo@icloud.com>
2024-12-09 22:18:48 +0900

    設定類

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_07_091227) do
+ActiveRecord::Schema.define(version: 2024_12_08_042946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "schedule_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["schedule_id"], name: "index_favorites_on_schedule_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.text "body"
@@ -72,6 +81,8 @@ ActiveRecord::Schema.define(version: 2024_12_07_091227) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "schedules"
+  add_foreign_key "favorites", "users"
   add_foreign_key "schedule_transportations", "schedules"
   add_foreign_key "schedule_transportations", "transportations"
   add_foreign_key "schedules", "users"
