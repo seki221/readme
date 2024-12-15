01c0a7d739f163219795259ae1113ea4510f5f13
seki221 <pawls00.bugaboo@icloud.com>
2024-12-15 23:51:09 +0900

    add commit "planners"

---
+++ app/controllers/planners_controller.rb
@@ -0,0 +1,32 @@
+class PlannersController < ApplicationController
+  before_action :authenticate_user! # Deviseを使用している場合
+
+  def index
+    @planner = Planner.all.order(date: :asc)
+  end
+
+  def new
+    @planner = Planner.new
+  end
+
+  def create
+    @planner = current_user.planner.new(planner_params)
+
+    if @planner.save
+      redirect_to @planner, notice: t('defaults.flash_message.created')
+    else
+      render :new
+    end
+  end
+
+  def show
+    @planner = Planner.find(params[:id]) 
+    @schedules = @planner.schedules
+  end
+
+  private
+  
+  def planner_params
+    params.require(:planner).permit(:title, :start_date, :end_date)
+  end
+end
