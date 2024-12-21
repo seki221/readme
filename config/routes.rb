306c9806cf970e3e8bcc944b0360ca0a5fc214ad
seki221 <pawls00.bugaboo@icloud.com>
2024-12-21 14:30:09 +0900

    routes簡素化

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root to: 'schedules#index'
-  resources :schedules, only: %i[index new create show edit update destroy] do
-    # resources :comments, only: %i[update create edit destroy], shallow: true
-      collection do
-        get :favorite
-        get 'search'
-      end
-    resources :reviews, only: %i[new create]
-    member do
-      get :next_step
+  root to: 'planners#index'
+
+  resources :planners, only: %i[index new create show edit update destroy] do
+    resources :schedules, only: %i[index new create show edit update destroy] do
     end
   end
-  
-  
-  get '/dates/:date', to: 'dates#show', as: 'date_show'
-
-  resources :favorite, only: %i[create destroy], shallow: true
 end
