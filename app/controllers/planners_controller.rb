d0f9c77edf1f4b6022f268e0bb82b6c9736ea128
seki221 <pawls00.bugaboo@icloud.com>
2024-12-22 16:40:45 +0900

    plannerのedit追加

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -12,7 +12,6 @@ class PlannersController < ApplicationController
 
   def create
     @planner = current_user.planner.new(planner_params)
-
     if @planner.save
       redirect_to @planner, notice: t('defaults.flash_message.created')
     else
@@ -24,6 +23,32 @@ class PlannersController < ApplicationController
     @planner = Planner.find(params[:id]) 
     @schedules = @planner.schedules
   end
+
+  def update
+    @planner = current_user.planners.find(params[:id])
+    if @planner.update(planner_params)
+      redirect_to @planner, notice: 'Planner was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def edit
+  @planner = current_user.planners.find(params[:id])
+  end
+
+
+  def destroy
+    @planner = current_user.planners.find_by(id: params[:id])
+    if @planner
+      @planner.destroy!
+      redirect_to planners_path, status: :see_other, notice: t('defaults.flash_message.deleted', item: Planner.model_name.human)
+    else
+      redirect_to planners_path, status: :not_found, alert: t('defaults.flash_message.not_found', item: Planner.model_name.human)
+    end
+  end
+
 
   private
   
