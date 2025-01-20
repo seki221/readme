878aaf8dc7062173f3617a1ac192699e4ddd140b
seki221 <pawls00.bugaboo@icloud.com>
2025-01-20 23:58:07 +0900

    add : admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,32 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # root to: 'home#top'
+  
+  # davise関係
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  
   devise_scope :user do
-    # post "oauth/callback", to: "oauths#callback"
-    # get "oauth/callback", to: "oauths#callback"
-    # get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
-    # delete '/users/sign_out' => 'devise/sessions#destroy'
     get '/users/sign_in'
-    # post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root to: 'planners#index'
 
+  # OAuthログイン関連
+  post 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
+
+
+  # 管理者関連
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  namespace :admin do
+    resources :dashboard
+    resources :users, only: %i[index edit update destroy] do
+    end
+  end
+
+  # planページ
   resources :planners, only: %i[index new create show edit update destroy] do
     resources :schedules, only: %i[index new create show edit update destroy] do
     end
