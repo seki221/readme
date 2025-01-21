aaa50079e8896f73d495e9dfb036fb3a1462e4a2
seki221 <pawls00.bugaboo@icloud.com>
2025-01-21 23:59:57 +0900

    add require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
+  before_action :require_login
 
   private
   def require_login
