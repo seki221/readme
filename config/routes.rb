35084288675088d4dbe8622f0ea60153b02386e6
seki221 <pawls00.bugaboo@icloud.com>
2025-01-25 00:05:03 +0900

    chenge routes[devise_scope]

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root to: 'home#top'
-  
   # davise関係
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  devise_scope :user do
-    get '/users/sign_in'
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  # devise_scope :user do
+  #   get '/users/sign_in'
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  
+  # end
 
   # OAuthログイン関連
   post 'oauth/callback', to: 'oauths#callback'
