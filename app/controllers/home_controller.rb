7484668ae0a9dbc0c6c1ed1c7d06fcaf18022ed4
seki221 <pawls00.bugaboo@icloud.com>
2025-01-25 19:59:35 +0900

    add toppage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  # skip_before_action :require_login, only: %i[top]
 
-  def top; end
+  def top
+    @planners = Planner.order(created_at: :desc).limit(5)
+  end
+
 end
