dc207e10f13d4fdd15c947aed12687fad9242336
seki221 <pawls00.bugaboo@icloud.com>
2025-01-29 22:37:23 +0900

    update routes:admin,mypage

--- config/routes.rb
+++ config/routes.rb
@@ -22,17 +22,24 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :admin do
     root "dashboards#index"
-    resources :dashboard, only: %i[index]
-    get 'login' => 'user_sessions#new', :as => :login
-    post 'login' => "user_sessions#create"
-    delete 'logout' => 'user_sessions#destroy', :as => :logout
-    resources :users, only: %i[index edit update destroy] do
-    end
+    resources :dashboards, only: [:index]
+    resources :users, only: [:index, :edit, :update, :destroy]
+    resources :user_sessions, only: [:new, :create, :destroy], path_names: { new: "login", destroy: "logout" }
   end
 
   # planページ
   resources :planners, only: %i[index new create show edit update destroy] do
     resources :schedules, only: %i[index new create show edit update destroy] do
     end
+  end
+
+  #mypage
+  resource :mypage, only: %i[show edit update] do
+    get 'submit_plans', to: 'users#submit_plans'
+    get 'evaluated_plans', to: 'users#evaluated_plans'
+    get 'email', to: 'users#edit_email', as: :edit_email
+    patch 'email', to: 'users#update_email'
+    get 'password', to: 'users#edit_password', as: :edit_password
+    patch 'password', to: 'users#update_password'
   end
 end
