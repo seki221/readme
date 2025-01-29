8fc0d86dcdedb253775fd375d3e29769d74fdb63
seki221 <pawls00.bugaboo@icloud.com>
2025-01-29 22:26:07 +0900

    add admin

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,22 +1,19 @@
 class Admin::UserSessionsController < Admin::BaseController
   skip_before_action :require_login, only: %i[new create]
   skip_before_action :check_admin, only: %i[new create]
-  layout 'layouts/admin_login'
-
-  def new; end
+  layout 'admin_login'
 
   def create
-    @user = login(params[:email], params[:password])
-    if @user
-      redirect_to admin_root_path, success: t('.success')
+    if (@user = login(params[:email], params[:password]))
+      redirect_to admin_root_path, notice: t('.success')
     else
-      flash.now[:danger] = t('.failure')
+      flash.now[:alert] = t('.failure')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to admin_login_path, status: :see_other, danger: t('.success')
+    redirect_to admin_login_path, notice: t('.logout_success')
   end
 end
--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,21 +2,15 @@ class MypagesController < ApplicationController
   before_action :authenticate_user! 
   before_action :correct_user, only: %i[index show edit update destroy]
 
-  def index
-    @planners = current_user.planners
-  end
-
   def show
-    @planner = current_user.planners.find_by(id: params[:id])
-    if @planner.nil?
-      redirect_to planners_path
-    end
+    @planners = current_user.planners
+
   end
 
   private
 
   def correct_user
     @planner = current_user.planners.find_by(id: params[:id])
-    redirect_to root_path
+    redirect_to root_path, alert: "権限がありません" unless current_user
   end
 end
